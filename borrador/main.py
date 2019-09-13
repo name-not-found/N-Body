f157b6257dc6f342cf51c9f9f066eba1333d2f44
icaribe <larry1313stylinson@gmail.com>
2019-09-13 12:08:25 -0500

    uploading .A

---
+++ borrador/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+# n-body.py solve the n-body problem using Newton
+# Copyright (C) <2019>  Angelica Nayeli Rivas Bedolla (angelica.nayeli@comunidad.unam.mx)
+#                       Pablo Clemente Moreno (clemnte@comunidad.unam.mx)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+# files
+import classes
+
+#libraries
+import math
+
+if __name__ == "__main__":
+	p0 = [0., 0., 0.] #m
+	v0 = [1., 1., 1.] #m/s
+	m = 1.            #kg
+	
+	p1 = [10., 0., 0.] #m
+	v1 = [0., 0., 0.] #m/s
+	m1 = 1e24            #kg
+	
+	dt = 1.           #sec
+
+	A = classes.Particle(p0, v0, m)
+	print(A.getPosition())
+
+	for t in range(60):
+		#print(A.getPosition())
+
+		A.integrate(dt,p1,m1)
+
+		if t%3 == 2:
+			print(A.getPosition())
