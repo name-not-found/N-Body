b252a947b60c2f944ac2d0dc7973e8a3d1c85775
Angelica <angelica.nayeli@comunidad.enes.mx>
2019-09-20 11:19:37 -0500

    uploading .A

--- borrador/graphics.py
+++ borrador/graphics.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
-def plotting(fig, ax, time, y1, titulo, filename, caption, xlabel, ylabel):
+def plotting2D(fig, ax, time, y1, titulo, filename, caption, xlabel, ylabel):
 	ax.plot(time, y1, label=caption)
 	ax.set(xlabel=xlabel, ylabel=ylabel,  title=titulo)
 	ax.grid()
 
-def plotting3D(fig, ax, y, m="o"):
+def plotting3D(fig, ax, y, i, m="o"):
+	c = ["g", "r"]
 	for point in y:
-		ax.scatter(point[0], point[1], point[2], marker=m)
+		ax.scatter(point[0], point[1], point[2], marker=m, c=c[i])
