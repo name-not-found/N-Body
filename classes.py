4f470104c0d2e7872c7e48f507bac97fa97b3960
Pablo <pablocm1998@gmail.com>
2019-11-07 19:58:49 -0600

    classes.py file update -P

--- classes.py
+++ classes.py
@@ -1,7 +1,8 @@
 import math
 #particle composition
 class Particle:
-	G = 3.9648449178363156e-14 # [UA, MS, S**2]     
+	#G = 3.9648449178363156e-14 # [UA, MS, S**2
+	G = 6.674e-11  # N*m**2*kg**-2      
 	def __init__(self, p, v, m, dt=1):
 		self.p = p #position
 		self.v = v #velocity
@@ -17,27 +18,27 @@ class Particle:
 		
 		Vx = (self.G*B.m*self.dt/(r**3))*u[0]
 		Vy = (self.G*B.m*self.dt/(r**3))*u[1]
-		# Vz = (self.G*B.m*self.dt/(r**3))*u[2]
+		Vz = (self.G*B.m*self.dt/(r**3))*u[2]
 
 		self.v[0] += Vx
 		self.v[1] += Vy
-		# self.v[2] += Vz
+		self.v[2] += Vz
 		
-		self.p = [self.p[0]+(self.v[0])*self.dt, self.p[1]+(self.v[1])*self.dt,] #self.p[2]+(self.v[2])*self.dt]
+		self.p = [self.p[0]+(self.v[0])*self.dt, self.p[1]+(self.v[1])*self.dt, self.p[2]+(self.v[2])*self.dt]
 
 	def getPosition(self):
 		return self.p
 
 	def getKineticEnergy(self):
-		k = (1/2)*self.m*( math.sqrt( self.v[0]**2 + self.v[1]**2))# + self.v[2]**2) )
+		k = (1/2)*self.m*( math.sqrt( self.v[0]**2 + self.v[1]**2 + self.v[2]**2) )
 		return k
 	
 	def computeR(self,p1):
-		r= math.sqrt( (p1[0]-self.p[0])**2 + (p1[1]-self.p[1])**2 )# + (p1[2]-self.p[2])**2 ) 
+		r= math.sqrt( (p1[0]-self.p[0])**2 + (p1[1]-self.p[1])**2 + (p1[2]-self.p[2])**2 ) 
 		return r
 	
 	def computeU(self, p1):
-		u=[0,0]
+		u=[0,0,0]
 		i=0
 		for a,b in zip(self.p, p1):
 			u[i] = b - a
@@ -57,17 +58,17 @@ class Particle:
 		
 		Vx = (self.G*B.m*self.dt/(r**3))*u[0]
 		Vy = (self.G*B.m*self.dt/(r**3))*u[1]
-		# Vz = (self.G*B.m*self.dt/(r**3))*u[2]
+		Vz = (self.G*B.m*self.dt/(r**3))*u[2]
 	 	
-		return [Vx,Vy,]#Vz]
+		return [Vx,Vy,Vz]
 
 	def updateV(self, v):
 		self.v[0] += v[0]
 		self.v[1] += v[1]
-		# self.v[2] += v[2]
+		self.v[2] += v[2]
 
 	def updatePosition(self, time,save):
-		self.p = [self.p[0]+(self.v[0])*self.dt, self.p[1]+(self.v[1])*self.dt,]# self.p[2]+(self.v[2])*self.dt]
+		self.p = [self.p[0]+(self.v[0])*self.dt, self.p[1]+(self.v[1])*self.dt, self.p[2]+(self.v[2])*self.dt]
 		if save:
 			self.time.append(time)
 			self.trajectory.append(self.p)
